bfadd64c6c549f201be51c31848959e1e9e1d91d
Waylan Hedine <hedin090@umn.edu>
2019-08-12 22:58:14 -0500

    refactored
    fixed duplication message

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ define(function(require, exports, module) {
           ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
           ModalDialog = require("text!modal/modal.html");
 	
-    const extension = 'waymans-project-helper',
-          charset = 'utf8',
-          tempFolder = '/templates/',
-          slash = '/',
-          keyBinds = [
+    const EXTENSION_NAME = 'waymans-project-helper',
+          CHARSET = 'utf8',
+          TEMPLATE_FOLDER = '/templates/',
+          BACKSLASH = '/',
+          KEYBINDS = [
               { "key": 'Ctrl-Shift-T', "platform": 'win' },
               { "key": 'Cmd-Shift-T', "platform": 'mac' }
           ];
@@ -23,25 +23,25 @@ define(function(require, exports, module) {
     let projectRoot,
         templatePath,
         templateList;
+    
+    // gets user extensions folder for templates
+    setTimeout(function(){
+        let installedExts = new ExtensionManagerViewModel.InstalledViewModel();
+        installedExts.initialize().then(function() {
+            templatePath = installedExts.extensions[EXTENSION_NAME].installInfo.path;
+            getTemplateList();
+        });
+    }, 1000);
 	
     function getProjectRoot() {
         projectRoot = ProjectManager.getProjectRoot()._path;
     }
 	
     function getTemplateList() {
-        brackets.fs.readdir(templatePath + tempFolder, function(err, data){
+        brackets.fs.readdir(templatePath + TEMPLATE_FOLDER, function(err, data){
             templateList = data;
         });
     }
-	
-    function getPathForTemplates() {
-        let installedExts = new ExtensionManagerViewModel.InstalledViewModel();
-        installedExts.initialize().then(function() {
-            templatePath = installedExts.extensions[extension].installInfo.path;
-            getTemplateList();
-        });
-    }
-    setTimeout(getPathForTemplates, 1000);
 
     function templateHandler(template) {
         let editor = EditorManager.getActiveEditor();
@@ -50,22 +50,97 @@ define(function(require, exports, module) {
             editor.document.replaceRange(template, insertionPos);
         }
     }
-	
-    function folderMaker(folder) {
+    
+    function splitFolders(folder) {
         let folders = folder.split('/'),
             last = folders.splice(folders.length - 1, 1)[0];
         folders = folders.join('/');
-        ProjectManager.createNewItem(projectRoot + folders, last, true, true);
+        return {start: folders, end: last};
+    }
+    
+    function fileMaker(folder, file) {
+        ProjectManager.createNewItem(projectRoot + folder, file, true, false);
     }
 	
-    function fileMaker(folder, file, template) {
-        ProjectManager.createNewItem(projectRoot + folder, file, true, false);
+    function folderMaker(folder) {
+        let path = splitFolders(folder);
+        ProjectManager.createNewItem(projectRoot + path.start, path.end, true, true);
+    }
+	
+    function getTemplateFolder(template) {
         brackets.fs.readdir(
-            templatePath + tempFolder + template, function(err, data){
-                brackets.fs.readFile(templatePath + tempFolder + template + slash + data[0], charset, function(err, text){
-                    setTimeout(() => templateHandler(text), 50);
-                });
+            templatePath + TEMPLATE_FOLDER + template, function(err, file){
+                getTemplate(template, file[0]);
             });
+    }
+    
+    function getTemplate(template, file) {
+        brackets.fs.readFile(templatePath + TEMPLATE_FOLDER + template + BACKSLASH + file, CHARSET, function(err, text){
+            setTimeout(() => templateHandler(text), 50);
+        });
+    }
+    
+    // using conditionals for now
+    function conditionals(file, folder, template) {
+        let isFolder = folder && typeof folder === 'string',
+            isFile = file && typeof file === 'string',
+            isTemplate = file && typeof file === 'string';
+        
+        function conditionalTemplate() {
+            // if user typed in template
+            if (isTemplate) {
+                // paste template
+                getTemplateFolder(template);
+            }
+        }
+        function conditionalFile() {
+            // make file
+            fileMaker(folder, file);
+            conditionalTemplate();
+        }
+        function conditionalFocus() {
+            // focus on file
+            console.log('code me'); // add solution
+            conditionalTemplate();
+        }
+        
+        // if user typed in folder
+        if (isFolder) {
+            // lookup folder
+            brackets.fs.readdir(projectRoot + folder, function(err, data){
+                // if it does not exist
+                if (err !== 0) {
+                    // make folder
+                    folderMaker(folder);
+                    // if user typed in file
+                    if (isFile) {
+                        conditionalFile();
+                    }
+                // if it exists
+                } else {
+                    // if user typed in file and folder contains file
+                    if (isFile && !data.includes(file)) {
+                        conditionalFile();
+                    // if user typed in file and folder does not contain file
+                    } else if (isFile && data.includes(file)) {
+                        conditionalFocus();
+                    }
+                }
+            });
+        } else {
+            if (isFile) {
+                // lookup file
+                brackets.fs.readdir(projectRoot, function(err, data){
+                    // if it does not exist
+                    if (!data.includes(file)) {
+                        conditionalFile();
+                    // if it exists
+                    } else {
+                        conditionalFocus();
+                    }
+                });
+            }
+        }
     }
 	
     function decisionMaker() {
@@ -77,21 +152,14 @@ define(function(require, exports, module) {
             file = $('#file', modal),
             folder = $('#folder', modal),
             template = $('#template', modal);
-		
+
         decision.done(function(choice){
             if (choice === 'ok') {
                 file = file.val();
                 folder = folder.val();
                 template = template.val();
-				
-                if (!file) {
-                    folderMaker(folder);
-                } else {
-                    if (folder) {
-                        folderMaker(folder);	
-                    }
-                    fileMaker(folder, file, template);
-                }
+                
+                conditionals(file, folder, template);
             }
         });
     }
@@ -102,7 +170,7 @@ define(function(require, exports, module) {
     CommandManager.register(
         'Project Helper', 'waymans.helper', () => decisionMaker()
     );
-    menu.addMenuItem('waymans.helper', keyBinds);
+    menu.addMenuItem('waymans.helper', KEYBINDS);
 	
     ExtensionUtils.loadStyleSheet(module, 'styles/styles.css');
 	
